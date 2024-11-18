dd4dca8a4ec0d63ba19160a5871c8fd403307ab5
JOÃO RAFAEL DA CUNHA GUERRA <8200098@estg.ipp.pt>
2024-11-18 18:11:39 +0000

    +

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25"
+
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -73,38 +74,13 @@ dependencies {
         val nav_version = "2.8.3"
         implementation("androidx.navigation:navigation-compose:$nav_version")
         implementation("androidx.compose.material:material-icons-extended:1.5.0")
-        implementation ("androidx.room:room-runtime:2.5.1")
-    val room_version = "2.6.1"
 
-    implementation("androidx.room:room-runtime:$room_version")
-
-    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
-    // See Add the KSP plugin to your project
-    implementation("com.google.dagger:dagger-compiler:2.51.1")
-    ksp("com.google.dagger:dagger-compiler:2.51.1")
-
-    // If this project only uses Java source, use the Java annotationProcessor
-    // No additional plugins are necessary
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-
-    // optional - Kotlin Extensions and Coroutines support for Room
-    implementation("androidx.room:room-ktx:$room_version")
-
-    // optional - RxJava2 support for Room
-    implementation("androidx.room:room-rxjava2:$room_version")
-
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$room_version")
-
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
-
-    // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
-
-
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+    annotationProcessor("androidx.room:room-compiler:2.6.0")
+    kapt("androidx.room:room-compiler:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.firebase:firebase-auth:22.0.0")
 
 }
--- app/src/main/java/com/example/trabalhocmu/ui/screen/RegisterScreen.kt
+++ app/src/main/java/com/example/trabalhocmu/ui/screen/RegisterScreen.kt
@@ -10,7 +10,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,38 +21,50 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.trabalhocmu.ui.component.BackgroundWithImage
 import com.example.trabalhocmu.R
 import com.example.trabalhocmu.room.entity.AppDatabase
 import com.example.trabalhocmu.room.entity.User
 import com.example.trabalhocmu.ui.theme.PoppinsFamily
-
 import kotlinx.coroutines.launch
+
 
 @Composable
 fun RegisterScreen(navController: NavController) {
+    val scrollState = rememberScrollState()
+    val currentLanguage = remember { mutableStateOf("PT") }
+
+    // Criando variáveis de estado para os campos de texto
+    val name = remember { mutableStateOf("") }
+    val username = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val age = remember { mutableStateOf(0) }
+    val gender = remember { mutableStateOf("") }
+    val mobileNumber = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val confirmPassword = remember { mutableStateOf("") }
+    // Estado para controlar a visibilidade da senha
+    val passwordVisible = remember { mutableStateOf(false) }
+    val confirmPasswordVisible = remember { mutableStateOf(false) }
+
     val context = LocalContext.current
     val db = AppDatabase.getDatabase(context).userDao()
     val coroutineScope = rememberCoroutineScope()
 
-    val name = remember { mutableStateOf("") }
-    val username = remember { mutableStateOf("") }
-    val email = remember { mutableStateOf("") }
-    val password = remember { mutableStateOf("") }
-    val confirmPassword = remember { mutableStateOf("") }
-    val passwordVisible = remember { mutableStateOf(false) }
-    val confirmPasswordVisible = remember { mutableStateOf(false) }
-    val currentLanguage = remember { mutableStateOf("PT") }
 
-    Box(modifier = Modifier.fillMaxSize()) {
+
+
+    BackgroundWithImage {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
+                .verticalScroll(scrollState) // Adiciona rolagem ao formulário completo
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -61,92 +76,142 @@ fun RegisterScreen(navController: NavController) {
             )
             Spacer(modifier = Modifier.height(20.dp))
 
+            // Título de Registro
             Text(
                 text = if (currentLanguage.value == "PT") "Cadastrar" else "Register",
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
                 fontFamily = PoppinsFamily
             )
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Subtítulo
+            Text(
+                text = if (currentLanguage.value == "PT") "Preencha seus dados para cadastrar" else "Enter your details to register",
+                fontFamily = PoppinsFamily
+            )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // Nome
             OutlinedTextField(
                 value = name.value,
                 onValueChange = { name.value = it },
-                label = { Text("Name") },
+                label = { Text(text = if (currentLanguage.value == "PT") "Nome" else "Name", fontFamily = PoppinsFamily) },
                 shape = RoundedCornerShape(12.dp),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // Nome de usuário
             OutlinedTextField(
                 value = username.value,
                 onValueChange = { username.value = it },
-                label = { Text("Username") },
+                label = { Text(text = if (currentLanguage.value == "PT") "Nome de Usuário" else "Username", fontFamily = PoppinsFamily) },
                 shape = RoundedCornerShape(12.dp),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // Email
             OutlinedTextField(
                 value = email.value,
                 onValueChange = { email.value = it },
-                label = { Text("Email") },
+                label = { Text(text = if (currentLanguage.value == "PT") "Email" else "Email Address", fontFamily = PoppinsFamily) },
                 shape = RoundedCornerShape(12.dp),
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // Idade
+            OutlinedTextField(
+                value = age.value,
+                onValueChange = { age.value = it },
+                label = { Text(text = if (currentLanguage.value == "PT") "Idade" else "Age", fontFamily = PoppinsFamily) },
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Gênero
+            OutlinedTextField(
+                value = gender.value,
+                onValueChange = { gender.value = it },
+                label = { Text(text = if (currentLanguage.value == "PT") "Gênero" else "Gender", fontFamily = PoppinsFamily) },
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Número de celular
+            OutlinedTextField(
+                value = mobileNumber.value,
+                onValueChange = { mobileNumber.value = it },
+                label = { Text(text = if (currentLanguage.value == "PT") "Número de Celular" else "Mobile Number", fontFamily = PoppinsFamily) },
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            // Senha com ícone para mostrar/ocultar
             OutlinedTextField(
                 value = password.value,
                 onValueChange = { password.value = it },
-                label = { Text("Password") },
+                label = { Text(text = if (currentLanguage.value == "PT") "Senha" else "Password", fontFamily = PoppinsFamily) },
                 visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
                 shape = RoundedCornerShape(12.dp),
                 trailingIcon = {
+                    // Ícone de olho para mostrar/ocultar a senha
                     IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
                         Icon(
                             imageVector = if (passwordVisible.value) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
-                            contentDescription = "Toggle Password Visibility"
+                            contentDescription = if (passwordVisible.value) "Hide password" else "Show password"
                         )
                     }
                 },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
             )
 
             Spacer(modifier = Modifier.height(10.dp))
 
+            // Confirmar Senha com ícone para mostrar/ocultar
             OutlinedTextField(
                 value = confirmPassword.value,
                 onValueChange = { confirmPassword.value = it },
-                label = { Text("Confirm Password") },
+                label = { Text(text = if (currentLanguage.value == "PT") "Confirmar Senha" else "Confirm Password", fontFamily = PoppinsFamily) },
                 visualTransformation = if (confirmPasswordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
                 shape = RoundedCornerShape(12.dp),
                 trailingIcon = {
+                    // Ícone de olho para mostrar/ocultar a senha
                     IconButton(onClick = { confirmPasswordVisible.value = !confirmPasswordVisible.value }) {
                         Icon(
                             imageVector = if (confirmPasswordVisible.value) Icons.Filled.Visibility else Icons.Filled.VisibilityOff,
-                            contentDescription = "Toggle Confirm Password Visibility"
+                            contentDescription = if (confirmPasswordVisible.value) "Hide password" else "Show password"
                         )
                     }
                 },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth() // Faz com que o campo ocupe toda a largura
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
+
             Button(
                 onClick = {
+                    val ageValue = age.value.toIntOrNull() ?: 0
                     val user = User(
                         name = name.value,
                         username = username.value,
                         email = email.value,
-                        age = 25,  // Pode ser uma entrada extra
-                        gender = "Male",  // Pode ser uma entrada extra
-                        mobileNumber = "1234567890",  // Pode ser uma entrada extra
+                        age = ageValue, // Pode ser uma entrada extra
+                        gender = gender.value,  // Pode ser uma entrada extra
+                        mobileNumber = mobileNumber.value,  // Pode ser uma entrada extra
                         password = password.value
                     )
                     coroutineScope.launch {
@@ -159,6 +224,37 @@ fun RegisterScreen(navController: NavController) {
             ) {
                 Text(text = if (currentLanguage.value == "PT") "Próximo" else "Next")
             }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = if (currentLanguage.value == "PT") "Já tem uma conta? Faça login" else "Do you have an account? Login",
+                modifier = Modifier.clickable {
+                    navController.navigate("Login")
+                }
+            )
+
+            // Seletor de idioma dentro do scroll, no canto inferior
+            Row(
+                modifier = Modifier
+                    .align(Alignment.End) // Alinha o seletor de idioma no canto inferior direito
+                    .clickable {
+                        currentLanguage.value = if (currentLanguage.value == "PT") "ENG" else "PT"
+                    },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = if (currentLanguage.value == "PT") "PT | ENG" else "ENG | PT",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRegisterScreen() {
+    RegisterScreen(navController = rememberNavController())
 }
